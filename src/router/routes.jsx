12e72a9d142e4d239ea0d40ab097024c8de7ca06
Ivan <djsupersky@gmail.com>
2023-01-30 18:56:02 +0300

    try to fix routing in github pages

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -5,6 +5,7 @@ import Error from "../pages/Error";
 import Landing from "../pages/Landing";
 import Course from "../pages/Course";
 import MyProgress from "../pages/MyProgress";
+import Tests from "../pages/Tests";
 
 export const publicRoutes = [
     {path: '/welcome', component: <Landing/>, exact: true},
@@ -14,8 +15,9 @@ export const publicRoutes = [
 ]
 
 export const privateRoutes = [
-    {path: '/courses', component: <Courses/>, exact: true},
-    {path: '/courses/java', component: <Course/>, exact: true},
-    {path: '/progress', component: <MyProgress/>, exact: true},
-    {path: '*', component: <Navigate to='/courses'/>, exact: true}
+    {path: '/courses', component: <Courses/>},
+    {path: '/courses/java', component: <Course/>},
+    {path: '/tests', component: <Tests/>},
+    {path: '/progress', component: <MyProgress/>},
+    {path: '*', component: <Navigate to='/courses'/>}
 ]
